15d5006b01de62c1ae8c05720567f6414a1c6cf6
Robin Liao <yilia@microsoft.com>
2019-04-01 18:23:58 -0700

    add adaptive card and adaptiveBotBuilderAction

--- CSharp/Microsoft.Bot.Schema.Teams/CardExtensions.cs
+++ CSharp/Microsoft.Bot.Schema.Teams/CardExtensions.cs
@@ -4,6 +4,9 @@
 
 namespace Microsoft.Bot.Schema.Teams
 {
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
     /// <summary>
     ///  Card extension methods.
     /// </summary>
@@ -49,6 +52,56 @@ namespace Microsoft.Bot.Schema.Teams
                 Content = card,
                 ContentType = FileConsentCard.ContentType,
             };
+        }
+
+        /// <summary>
+        /// Creates a new attachment from AdaptiveCard.
+        /// </summary>
+        /// <param name="card"> The instance of AdaptiveCard.</param>
+        /// <returns> The generated attachment.</returns>
+        public static Attachment ToAttachment(this AdaptiveCards.AdaptiveCard card)
+        {
+            return new Attachment
+            {
+                Content = card,
+                ContentType = AdaptiveCards.AdaptiveCard.ContentType,
+            };
+        }
+
+        /// <summary>
+        /// Creates a new attachment from AdaptiveCardParseResult.
+        /// </summary>
+        /// <param name="cardParsedResult"> The instance of AdaptiveCardParseResult that represents results parsed from JSON string.</param>
+        /// <returns> The generated attachment.</returns>
+        public static Attachment ToAttachment(this AdaptiveCards.AdaptiveCardParseResult cardParsedResult)
+        {
+            return cardParsedResult.Card.ToAttachment();
+        }
+
+        /// <summary>
+        /// Wrap BotBuilder action into AdaptiveCard.
+        /// </summary>
+        /// <param name="action"> The instance of adaptive card.</param>
+        /// <param name="targetAction"> Target action to be adapted.</param>
+        public static void RepresentAsBotBuilderAction(this AdaptiveCards.AdaptiveSubmitAction action, CardAction targetAction)
+        {
+            var wrappedAction = new CardAction
+            {
+                Type = targetAction.Type,
+                Value = targetAction.Value,
+                Text = targetAction.Text,
+                DisplayText = targetAction.DisplayText,
+            };
+
+            JsonSerializerSettings serializerSettings = new JsonSerializerSettings();
+            serializerSettings.NullValueHandling = NullValueHandling.Ignore;
+
+            string jsonStr = action.DataJson == null ? "{}" : action.DataJson;
+            JToken dataJson = JObject.Parse(jsonStr);
+            dataJson["msteams"] = JObject.FromObject(wrappedAction, JsonSerializer.Create(serializerSettings));
+
+            action.Title = targetAction.Title;
+            action.DataJson = dataJson.ToString();
         }
     }
 }
